a7b697344d02996f998ba4033e4749087d8a7f32
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-07 21:01:34 +0530

    Create Program3.java

---
+++ SimpleJavaPrograms/Program3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Program3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        if(m%10 == n%10){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
