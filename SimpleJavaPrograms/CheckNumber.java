7c83d63777699921883e8d79ead2dbad99928259
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-07 20:41:01 +0530

    Update CheckNumber.java
    A Simple program to check wheather the number is positive, negative, or zero

--- SimpleJavaPrograms/CheckNumber.java
+++ SimpleJavaPrograms/CheckNumber.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+class CheckNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==0){
+            System.out.println(n+" is Zero");
+        }
+        else if(n>0){
+            System.out.println(n+" is Positive");
+        }
+        else{
+            System.out.println(n+" is Negative");
+        }
+    }
+}
