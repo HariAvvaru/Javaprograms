f825b2adffbeb4156fcbcdfe6cf05a61ded13cf0
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-09 11:38:21 +0530

    Create Program8.java

---
+++ SimpleJavaPrograms/Program8.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Program8 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        switch(ch){
+            case 'R' | 'r':
+                System.out.println("Red");
+                break;
+            case 'B' | 'b':
+                System.out.println("Blue");
+                break;
+            case 'G' | 'g':
+                System.out.println("Green");
+                break;
+            case 'O' | 'o':
+                System.out.println("Orange");
+                break;
+            case 'Y' | 'y':
+                System.out.println("Yellow");
+                break;
+            case 'W' | 'w':
+                System.out.println("White");
+                break;
+        }
+    }
+}
