86cf07971117dce7257ee74db1b4052af73ec127
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-09 23:16:44 +0530

    Create Program4.java

---
+++ Arrayprog/Program4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Program4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] a=new int[]{65,66,67,68,69,99};
+        char ch;
+        for(int i=0;i<a.length;i++)
+        {
+            ch=(char)a[i];
+            System.out.print(ch+"\t");
+        }
+    }
+}
