6c6a69f0adc482b4d3f6894e35b76df2b36d9bca
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-09 22:58:55 +0530

    Update Program1.java

--- Arrayprog/Program1.java
+++ Arrayprog/Program1.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+class Program1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            sum+=a[i];
+        }
+        System.out.println("Sum is: "+sum);
+        System.out.println("Average is: "+sum/n);
+    }
+}
