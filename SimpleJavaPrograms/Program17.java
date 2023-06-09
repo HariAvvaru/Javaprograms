d8c1051c98208a4d6bd18bd6a2dab886e32dc48b
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-09 20:54:11 +0530

    Update Program17.java

--- SimpleJavaPrograms/Program17.java
+++ SimpleJavaPrograms/Program17.java
@@ -18,3 +18,24 @@ class Program17 {
         }
     }
 }
+or
+/*Using command line arguments*/
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        int n=Integer.parseInt(args[0]);
+        int m=n;
+        int k=0;
+        while(n!=0){
+            int dig=n%10;
+            k=k*10+dig;
+            n=n/10;
+        }
+        if(k==m){
+            System.out.print(m+" is a Palindrome");
+        }
+        else{
+            System.out.print(m+" is not a Palindrome");
+        }
+    }
+}
