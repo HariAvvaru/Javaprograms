d9ce6bf2d430fc9cdd027f68880eb642db48fa6b
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-09 20:48:24 +0530

    Update Program16.java

--- SimpleJavaPrograms/Program16.java
+++ SimpleJavaPrograms/Program16.java
@@ -1,13 +1,27 @@
 import java.util.*;
 class Program16 {
     public static void main(String[] args) {
-        //System.out.println("HI");
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int k=0;
         while(n!=0){
             int dig=n%10;
             k=k*10+dig;
+            n=n/10;
+        }
+        System.out.print(k);
+    }
+}
+or
+import java.util.*;
+class Program16 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String k="";
+        while(n!=0){
+            String dig=String.valueOf(n%10);
+            k+=dig;
             n=n/10;
         }
         System.out.print(k);
