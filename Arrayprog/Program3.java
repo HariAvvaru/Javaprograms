add53d6ad9fc07c3e6dbe2a7ce456a05f98f4c20
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-09 23:11:33 +0530

    Update Program3.java

--- Arrayprog/Program3.java
+++ Arrayprog/Program3.java
@@ -2,16 +2,16 @@ import java.util.*;
 class Program3 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int[] a=new int[]{2,1,5,6,8,3,4,7};
+        int[] a=new int[]{1,4,34,56,7};
         int target=sc.nextInt();
         int b=Arrays.binarySearch(a,target);
-        if(b>=0)
+        if(b<0)
         {
-            System.out.println(target+" is found");
+            System.out.println("-1");
         }
         else
         {
-            System.out.println(target+" is not found");
+            System.out.println(b+" is the index");
         }
     }
 }
