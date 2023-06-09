da87fe2176f458a17f56659b75adbb2aaa682da7
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-09 12:08:20 +0530

    Create Program10.java

---
+++ SimpleJavaPrograms/Program10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Program10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<n;i++){
+            System.out.print(i+"\t");
+        }
+        System.out.print(n);
+    }
+}
