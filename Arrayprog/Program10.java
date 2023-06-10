29b888c53086d09c851b60ad0cb62be7e87db075
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-10 14:08:16 +0530

    Create Program10.java

---
+++ Arrayprog/Program10.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Program10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int[] b=new int[n];
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(a[i]%2==0){
+                System.out.print(a[i]+"\t");
+            }
+            else{
+                b[j]=a[i];
+                a[i]=-1;
+                j++;
+            }
+        }
+        for(int i=0;i<j;i++){
+            System.out.print(b[i]+"\t");
+        }
+    }
+}
