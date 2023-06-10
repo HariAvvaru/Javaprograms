a9b81581d3998c454f6fda4d084c1fde94b1f15b
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-06-10 14:52:32 +0530

    Update Program12.java

--- Arrayprog/Program12.java
+++ Arrayprog/Program12.java
@@ -2,34 +2,21 @@ import java.util.*;
 class Program12 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        if(args.length!=4){
-            System.out.print("Please enter 4 integer numbers");
+        int[] a= new int[3];
+        int[] b=new int[3];
+        for(int i=0;i<3;i++){
+            a[i]=sc.nextInt();
         }
-        else{
-            printArr(args);
-            printrevArr(args);
+        for(int i=0;i<3;i++){
+            b[i]=sc.nextInt();
         }
+        middleWay(a,b);
     }
-    public static void printArr(String[] a){
-        int k=0;
-        System.out.println("The given array is ");
-        for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                System.out.print(a[k]+" ");
-                k+=1;
-            }
-            System.out.println();
-        }
-    }
-    public static void printrevArr(String[] a){
-        int k=4;
-        System.out.println("The reverse given array is ");
-        for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                k-=1;
-                System.out.print(a[k]+" ");
-            }
-            System.out.println();
-        }
+    public static void middleWay(int[] a,int[] b){
+        int k = (int) Math.ceil(3/2);
+        int[] c=new int[2];
+        c[0]=a[k];
+        c[1]=b[k];
+        System.out.print("["+c[0]+","+c[1]+"]");
     }
 }
